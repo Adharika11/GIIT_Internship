a12ea33cbcc043ea9797cf092221affd50dac2e7
Adharika <adhs.gs2003@gmail.com>
2024-07-19 17:07:40 +0530

    Interface Implementation

---
+++ src/main/java/crud/backend/GIIT_Solutions/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package crud.backend.GIIT_Solutions.service;
+
+import crud.backend.GIIT_Solutions.dto.EmployeeDto;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+}
---
+++ src/main/java/crud/backend/GIIT_Solutions/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,22 @@
+package crud.backend.GIIT_Solutions.service.impl;
+
+import crud.backend.GIIT_Solutions.dto.EmployeeDto;
+import crud.backend.GIIT_Solutions.entity.Employee;
+import crud.backend.GIIT_Solutions.mapper.EmployeeMapper;
+import crud.backend.GIIT_Solutions.repo.EmployeeRepository;
+import crud.backend.GIIT_Solutions.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+       Employee employee=EmployeeMapper.maptoEmployee(employeeDto);
+       Employee savedEmployee=employeeRepository.save(employee);
+       return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+}
