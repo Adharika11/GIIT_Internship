ed50fc518aab541003f58fea290ad7b8c866753a
Adharika <adhs.gs2003@gmail.com>
2024-07-19 15:38:07 +0530

    Initial commit

---
+++ src/main/java/crud/backend/GIIT_Solutions/GiitSolutionsApplication.java
@@ -0,0 +1,13 @@
+package crud.backend.GIIT_Solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiitSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiitSolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/crud/backend/GIIT_Solutions/GiitSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package crud.backend.GIIT_Solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiitSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
