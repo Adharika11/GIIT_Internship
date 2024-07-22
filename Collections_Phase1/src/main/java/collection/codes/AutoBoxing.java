a75b6f2f1f884f6587a2afc3c78865572236c051
Adharika <adhs.gs2003@gmail.com>
2024-07-22 15:45:03 +0530

    Autoboxing

--- Collections_Phase1/src/main/java/collection/codes/AutoBoxing.java
+++ Collections_Phase1/src/main/java/collection/codes/AutoBoxing.java
@@ -1,2 +1,17 @@
-package collection.codes;public class AutoBoxing {
+package collection.codes;
+
+public class AutoBoxing {
+    public static void main(String[] args) {
+        float f = 1.16f;
+        Float t =f;
+        System.out.println("After autoboxing float: "+t);
+
+        boolean b=true;
+        Boolean bl=b;
+        bl=false;
+        System.out.println("After autoboxing boolean: "+bl);
+
+    }
+
+
 }
