e981b0e1d6f0a1d6776af89f9b4401134eb90c96
Adharika <adhs.gs2003@gmail.com>
2024-07-22 15:49:05 +0530

    Autounboxing

---
+++ Collections_Phase1/src/main/java/collection/codes/AutoUnboxing.java
@@ -0,0 +1,18 @@
+package collection.codes;
+
+public class AutoUnboxing {
+    public static void main(String[] args) {
+        Byte t =1;
+        byte b=t;
+        System.out.println("After autoboxing float: "+b);
+
+        Long l =13456L;
+        long bl=l;
+        System.out.println("After autoboxing boolean: "+bl);
+
+    }
+
+
+}
+
+
